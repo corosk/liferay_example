1e04484afd1bc029d20fa50726acd1e8380e8067
corosk <corosk>
2016-12-12 23:02:53 +0900

    service.xml java file commit

--- active_portal-service/src/main/resources/META-INF/sql/indexes.sql
+++ active_portal-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,3 +1,8 @@
+create index IX_3628AA05 on active_companyBookmark (bookmarkId[$COLUMN_LENGTH:75$]);
+create index IX_D8C6CC12 on active_companyBookmark (bookmarkTitle[$COLUMN_LENGTH:75$]);
+create index IX_8F9D0769 on active_companyBookmark (bookmarkUrl[$COLUMN_LENGTH:75$]);
+create index IX_7143FF1A on active_companyBookmark (userId);
+
 create index IX_66C2E2FD on active_company_bookmark (bookmark_id[$COLUMN_LENGTH:75$]);
 create index IX_C3D2501A on active_company_bookmark (bookmark_title[$COLUMN_LENGTH:75$]);
 create index IX_7249ED71 on active_company_bookmark (bookmark_url[$COLUMN_LENGTH:75$]);
--- active_portal-service/src/main/resources/META-INF/sql/tables.sql
+++ active_portal-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,3 +1,16 @@
+create table active_companyBookmark (
+	companyId VARCHAR(75) null,
+	groupId VARCHAR(75) null,
+	bookmarkId VARCHAR(75) not null primary key,
+	bookmarkUrl VARCHAR(75) null,
+	bookmarkTitle VARCHAR(75) null,
+	userId LONG,
+	content VARCHAR(75) null,
+	createDate DATE null,
+	updateDate DATE null,
+	isDelete BOOLEAN
+);
+
 create table active_company_bookmark (
 	companyId VARCHAR(75) null,
 	groupId VARCHAR(75) null,
